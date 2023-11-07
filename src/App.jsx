3506505fad4241c9568e3c8a65987a0cc748e249
Reu-Amissah <reujoyamissah@gmail.com>
2023-11-07 12:04:40 +0000

    add login and landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./Pages";
+import Login from "./Pages/Login";
 
 const AppRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  }
-])
+    element: <Home />,
+    // element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
-
-export default AppRouter
+export default AppRouter;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+// import "./LandingPage.css";
+import Navbar from "../components/LandNavBar/Navbar";
+import Welcome from "../components/Welcome/Welcome";
+import Mission from "../components/Mission/Mission";
+import Programs from "../components/Programs/Programs";
+import ContactUs from "../components/ContactUs/ContactUs";
+import Footer from "../components/Footer/Footer";
 
 function Home() {
   return (
-    <div className='text-red-500'>HomePage</div>
-  )
+    <div className="body-div">
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <section className="welcome-section">
+        <Welcome></Welcome>
+      </section>
+      <section style={{ marginTop: "40px" }}>
+        <Mission></Mission>
+      </section>
+      <section style={{ marginTop: "40px" }}>
+        <Programs></Programs>
+      </section>
+      <section className="contact-us-section" style={{ marginTop: "40px" }}>
+        <ContactUs></ContactUs>
+      </section>
+      <section className="footer-section">
+        <Footer></Footer>
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
