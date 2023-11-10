9c3924980b7c89db1178dc224a6abefdd5cce1be
dickson anyaele <dicksonanyaele1234@gmail.com>
2023-11-09 12:46:44 -1200

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      // Add any custom aliases for your project
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      // Add more aliases as needed
+    },
+  },
+});
