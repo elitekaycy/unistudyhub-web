4675d437fe326759d5c403a2be08e785c6a7e041
dickson anyaele <dicksonanyaele1234@gmail.com>
2023-11-09 12:59:15 -1200

    start

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,37 +1,50 @@
-import React from "react";
-// import "./LandingPage.css";
-// import Navbar from "../components/HomeComps/LandNavBar/Navbar.jsx";
-// import Welcome from "../components/HomeComps/Welcome/Welcome.jsx";
-// import Mission from "../components/HomeComps/Mission/Mission.jsx";
-// import Programs from "../components/HomeComps/Programs/Programs.jsx";
-// import ContactUs from "../components/HomeComps/ContactUs/Contactus.jsx";
-// import Footer from "../components/HomeComps/Footer/Footer.jsx"
+// import React from "react";
+// // import "./LandingPage.css";
+// // import Navbar from "../components/HomeComps/LandNavBar/Navbar.jsx";
+// // import Welcome from "../components/HomeComps/Welcome/Welcome.jsx";
+// // import Mission from "../components/HomeComps/Mission/Mission.jsx";
+// // import Programs from "../components/HomeComps/Programs/Programs.jsx";
+// // import ContactUs from "../components/HomeComps/ContactUs/Contactus.jsx";
+// // import Footer from "../components/HomeComps/Footer/Footer.jsx"
 
-import { Navbar, Welcome, Mission, Programs, Contactus, Footer} from "../components";
+// import { Navbar, Welcome, Mission, Programs, Contactus, Footer} from "../components";
+
+// function Home() {
+//   return (
+//     <div className="body-div">
+//       <header>
+//        <Navbar />
+//       </header>
+//       <section className="welcome-section">
+//        <Welcome />
+//       </section>
+//       <section className="mt-8">
+//        <Mission />
+//       </section>
+//       <section className="mt-8">
+//         <Programs />
+//       </section>
+//       <section className="contact-us-section mt-8">
+//         <Contactus />
+//       </section>
+//       <section className="footer-section">
+//        <Footer />
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+import React from 'react'
 
 function Home() {
   return (
-    <div className="body-div">
-      <header>
-       <Navbar />
-      </header>
-      <section className="welcome-section">
-       <Welcome />
-      </section>
-      <section className="mt-8">
-       <Mission />
-      </section>
-      <section className="mt-8">
-        <Programs />
-      </section>
-      <section className="contact-us-section mt-8">
-        <Contactus />
-      </section>
-      <section className="footer-section">
-       <Footer />
-      </section>
+    <div>Home
+
+      <a href='/login'>login</a>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react'
-import path from 'path';
-import ViteCSS from 'vite-plugin-css';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), ViteCSS()],
-  resolve: {
-    alias: {
-      // Add any custom aliases for your project
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      // Add more aliases as needed
-    },
-  },
-});
+  plugins: [react()],
+})
